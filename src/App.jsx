e3ae05d8e097f5d32db4f8cc73bf63b7bdd8ace5
Monik Malviya <monikmalviya04gmail.com>
2025-09-03 21:30:25 +0530

    fetch the response from api

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-
-import './App.css'
+import './App.css';
 import ChatWindow from "./ChatWindow.jsx";
-import "./sidebar.css"
+import "./sidebar.css";
 import Sidebar from "./Sidebar.jsx";
-
+import { useState } from 'react';
 import { Mycontext } from './Mycontext.jsx';
+import { v1 as uuidv1 } from 'uuid';
 
 function App() {
-    const providerValues = {};
-  return (
-   <div className='app'>
-    <Mycontext.Provider value={providerValues}>
-      <Sidebar />
-    <ChatWindow />
-   
-   </Mycontext.Provider>
+  const [prompt, setPrompt] = useState("");
+  const [reply, setReply] = useState("");
+  const[currThreadId , setcurrThreadId]= useState(uuidv1());
+  const providerValues = {
+    prompt, setPrompt,
+    reply, setReply,
+    currThreadId, setcurrThreadId
+  };
 
-   </div>
-  )
+  return (
+    <div className="app">
+      <Mycontext.Provider value={providerValues}>
+        <Sidebar />
+        <ChatWindow />
+      </Mycontext.Provider>
+    </div>
+  );
 }
 
 export default App;
--- src/ChatWindow.jsx
+++ src/ChatWindow.jsx
@@ -1,15 +1,76 @@
-import React from 'react'
-import "./sidebar.css"
-import './App.css'
+import React, { useContext, useState } from 'react';
+import './ChatWindow.css';
+import { Mycontext } from './Mycontext';
+import axios from 'axios';
+import {ScaleLoader} from "react-spinners";
+
 
 const ChatWindow = () => {
-  return (
-  <div>
-     chat window
-  </div>
+         const { reply, setReply, prompt, setPrompt , currThreadId } = useContext(Mycontext);
+          const [loading , setLoading]  = useState(false);
+
+           const getReply = async () => {
+                     setLoading(true);
+              
+
+            try {
+            const res = await axios.post(
+                "http://localhost:7777/api/chat",
+                { message: prompt, threadId: currThreadId}, 
+             { headers: { "Content-Type": "application/json" }, withCredentials: true }
+                     );
+                          
+                      console.log(res.data);
+
+                       setReply(res.data.reply);
+                        setLoading(false);
+                    }
+
+                  catch (err) {   
+               console.log(err);
+                   }
+                     };
+
+            
+
+             return (
+           <div className='chatwindow'>
+            <div className='navbar'>
+           <span> QuickGpt <i className="fa-solid fa-chevron-down"></i></span>
+           
+               <div className='user'>
+             <div className='usericon'>
+            <span className="userIcon"><i className="fa-solid fa-user"></i></span>
+              </div>
+            </div>
+            </div>
+
+               <ScaleLoader color ="#fff" loading={loading}>
+
+               </ScaleLoader>
+            <div className='chatinput'>
+             <div className='inputbox'>
+             <input
+             placeholder="Ask anything"
+          value={prompt}
+          onChange={(e) => setPrompt(e.target.value)}
+           onKeyDown={(e)=> e.key==="Enter" ? getReply() : ""}
+          />
+
+              
+               <span id='submit'onClick={getReply}><i className="fa-solid fa-paper-plane"></i></span>
+               
+               </div>
+             
+              <p className='info'>QuickGpt can make mistakes. Check important info. See Cookie Preferences.</p>
+             
+             </div>
+            </div>
+    
+
 
 
   )
-}
+};
 export default ChatWindow;
 
